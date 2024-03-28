ee81730b6c9257eff2cfd4965655c5a589f705b6
Izzatbek <izzatsultanov77@gmail.com>
2024-03-28 23:43:23 +0500

    feat: Add routes and Add pages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript/base',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="container m-auto">
+      <div className="text-center text-[25px]">Async Race</div>
+      <div className="flex justify-center gap-3">
+        <Link to={"/"}>Garage</Link>
+        <Link to={"/winners"}>Winners</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
